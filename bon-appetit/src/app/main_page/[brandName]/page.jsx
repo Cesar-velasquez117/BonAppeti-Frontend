45e6153f8397274eab0b9d1f81feab31d440ec20
Cesar-velasquez117 <106538584+Cesar-velasquez117@users.noreply.github.com>
2024-05-02 22:18:50 -0500

    Implementation of the endpoint to fill the table with the reviews

--- bon-appetit/src/app/main_page/[brandName]/page.jsx
+++ bon-appetit/src/app/main_page/[brandName]/page.jsx
@@ -1,11 +1,13 @@
 'use client'
 import React, {useEffect, useState} from "react";
 import TurnBackHeader from "@/components/getBackHeader";
-import { fetchRating } from "@/functions/requests";
+import { fetchRating, fetchReviews } from "@/functions/requests";
 
 export default function BrandPage({ searchParams}) {
     const { brandName, logoImage, url } = searchParams;
     const [rating, setRating] = useState(null);
+    const [positiveReviews, setPositiveReviews] = useState([]);
+    const [negativeReviews, setNegativeReviews] = useState([]);
 
     useEffect(() => {
         const fetchRatingData = async () => {
@@ -17,10 +19,49 @@ export default function BrandPage({ searchParams}) {
                 console.error("Error fetching rating:", error);
             }
         };
-
         fetchRatingData();
-
     }, []);
+
+    useEffect(() => {
+        const fetchReviewsData = async () => {
+            try {
+                const reviewsData = await fetchReviews(url);
+                console.log(reviewsData);
+                const positiveReviewsData = reviewsData.positiveOpinionsData;
+                const negativeReviewsData = reviewsData.negativeOpinionsData;
+                console.log("Pase de la asignacion" + positiveReviewsData)
+                setPositiveReviews(positiveReviewsData);
+                setNegativeReviews(negativeReviewsData);
+
+                } catch (error) {
+                    fetchReviewsData
+                    console.error("Error fetching reviews:", error);
+                }
+            };
+            fetchReviewsData();
+        }, []);
+    
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       try {
+    //         // Fetch rating
+    //         const ratingData = await fetchRating(url);
+    //         setRating(ratingData);
+    
+    //         // Fetch reviews
+    //         const reviewsData = await fetchReviews(url);
+    //         const { positiveReviewsData, negativeReviewsData } = reviewsData;
+    
+    //         // Set positive and negative opinions
+    //         setPositiveReviews(positiveReviewsData);
+    //         setNegativeReviews(negativeReviewsData);
+    //       } catch (error) {
+    //         console.error("Error fetching data:", error);
+    //       }
+    //     };
+    
+    //     fetchData();
+    //   }, []);
 
     return (
         <div>
@@ -31,10 +72,38 @@ export default function BrandPage({ searchParams}) {
                 </div>
                 <div className="w-1/2 rounded-lg bg-neutral p-4">
                     <h1 className="font-crimson-pro font-bold text-xl">{brandName}</h1>
-                    <p className=" font-crimson-pro text-gray-700 mt-2">Descripción de la cadena...</p>
-                    <p className="font-crimson-pro text-gray-700 mt-2">Opinión general...</p>
-                    <p className="font-crimson-pro text-gray-700 mt-2">Rating: {rating}</p>
+                    <p className=" font-crimson-pro text-gray-700 mt-2">Description: ...</p>
+                    <p className="font-crimson-pro text-gray-700 mt-2">General opinion: ...</p>
+                    <p className="font-crimson-pro text-gray-700 mt-2">Rating: {rating} out of 5</p>
                 </div>
+            </div>
+            <div className="mt-8 mx-auto w-3/4">
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th className="font-crimson-pro text-gray-700 border-b-2">Positive Reviews</th>
+                            <th className="font-crimson-pro text-gray-700 border-b-2">Negative Reviews</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {/* Generate rows alternating between positive and negative reviews */}
+                    {positiveReviews.map((positiveReview, index) => (
+                        <tr key={index}>
+                            <td className="font-crimson-pro text-gray-700 border-b">{positiveReview}</td>
+                            <td className="font-crimson-pro text-gray-700 border-b">
+                            {negativeReviews[index] || ""}
+                            </td>
+                        </tr>
+                    ))}
+                    {/* Add adicional rows for negative reviews if necessary */}
+                    {negativeReviews.slice(positiveReviews.length).map((negativeReview, index) => (
+                        <tr key={index}>
+                            <td className="font-crimson-pro text-gray-700 border-b"></td>
+                            <td className="font-crimson-pro text-gray-700 border-b">{negativeReview}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
